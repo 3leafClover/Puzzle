fd6f727b7eba3bbb180225eb49da28be0838ff77
Clover <z.omran2007@gmail.com>
2023-11-13 18:20:04 +0200

    added side bar + made multiple tabs draggable

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-// Get the draggable div element and the drag handle
-var draggableElement = document.querySelector('.draggable');
-var dragHandle = document.querySelector('.drag-handle');
+// Get all elements with the class "draggable-tab"
+var draggableTabs = document.querySelectorAll('.draggable');
 
-// Variables to store the mouse position during dragging
-var offsetX, offsetY;
+// Loop through each tab and set up the drag functionality
+draggableTabs.forEach(function(draggableTab) {
+  var dragHandle = draggableTab.querySelector('.drag-handle');
+  var offsetX, offsetY;
 
-// Event listener for mouse down on the drag handle
-dragHandle.addEventListener('mousedown', function(e) {
-  // Calculate the offset between the mouse position and the top-left corner of the div
-  offsetX = e.clientX - draggableElement.getBoundingClientRect().left;
-  offsetY = e.clientY - draggableElement.getBoundingClientRect().top;
+  // Event listener for mouse down on the drag handle
+  dragHandle.addEventListener('mousedown', function(e) {
+    // Calculate the offset between the mouse position and the top-left corner of the tab
+    offsetX = e.clientX - draggableTab.getBoundingClientRect().left;
+    offsetY = e.clientY - draggableTab.getBoundingClientRect().top;
 
-  // Add event listeners for mouse move and mouse up
-  document.addEventListener('mousemove', dragElement);
-  document.addEventListener('mouseup', stopDragging);
+    // Add event listeners for mouse move and mouse up
+    document.addEventListener('mousemove', dragElement);
+    document.addEventListener('mouseup', stopDragging);
+  });
+
+  // Function to handle the dragging of the tab
+  function dragElement(e) {
+    // Calculate the new position of the tab based on the mouse position
+    var x = e.clientX - offsetX;
+    var y = e.clientY - offsetY;
+
+    // Set the new position of the tab
+    draggableTab.style.left = x + 'px';
+    draggableTab.style.top = y + 'px';
+  }
+
+  // Function to stop dragging when the mouse is released
+  function stopDragging() {
+    // Remove event listeners for mouse move and mouse up
+    document.removeEventListener('mousemove', dragElement);
+    document.removeEventListener('mouseup', stopDragging);
+  }
 });
 
-// Function to handle the dragging of the div
-function dragElement(e) {
-  // Calculate the new position of the div based on the mouse position
-  var x = e.clientX - offsetX;
-  var y = e.clientY - offsetY;
 
-  // Set the new position of the div
-  draggableElement.style.left = x + 'px';
-  draggableElement.style.top = y + 'px';
-}
+// Loop through each tab and set up the drag and close functionality
+draggableTabs.forEach(function(draggableTab) {
+  var closeButton = draggableTab.querySelector('.close');
 
-// Function to stop dragging when the mouse is released
-function stopDragging() {
-  // Remove event listeners for mouse move and mouse up
-  document.removeEventListener('mousemove', dragElement);
-  document.removeEventListener('mouseup', stopDragging);
-}
+  // Event listener for close button click
+  closeButton.addEventListener('click', function() {
+    // Remove the tab from the document
+    draggableTab.style.display="none";
+  });
+});
